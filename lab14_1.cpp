f6d4d8a881c220a5217896dc9991b52bb369c387
Lica <Tanapatlica@gmail.com>
2023-01-13 17:11:04 +0700

    lab 14 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,43 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A [],int N,double B []){
+    int i = 0;
+        while(i<N){
+            B[0]+=A[i];
+            i++;
+        } B[0] = B[0]/N;
+    i = 0;
+    double devia=0;
+        while(i<N){
+            devia += pow((A[i]-B[0]),2);
+            i++;
+        } B[1] = sqrt(devia/N);
+    i=0;
+    double geo = 1;
+        while(i<N){
+            geo *= A[i];
+            i++;
+        } B[2] = pow(geo,1.0000/N);
+    i=0;
+    double harmo = 0;
+        while(i<N){
+            harmo += 1/A[i];
+            i++;
+        } B[3] = N/harmo;
+    i=0;
+    double max = A[0];
+        while(i<N){
+                if(A[i]>=max) max = A[i];
+            i++;
+        }
+    B[4] = max;
+    i=0;
+    double min = A[0];
+        while(i<N){
+                if(A[i]<=min) min = A[i];
+            i++;
+        }
+    B[5] = min;
+}
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,38 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image [][M],int size, int x, int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if (sqrt(pow((i-x),2)+pow((j-y),2)) <= size-1){
+                image [i][j] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool image [N][M]){
+    int i = 0;
+    while (i<=71){
+        cout << "-";
+    i++;
+    } cout << "\n";
+    int j = 0;
+    while (j<N){
+        i = 0;
+        cout << "|";
+        while (i<M){
+        if (image[j][i]==1){
+            cout << "*";
+        } else cout << " ";
+        i++;
+        } cout << "|\n";
+        j++;
+    }
+    i = 0;
+    while (i<=71){
+        cout << "-";
+    i++;
+    } cout << "\n";
+}
